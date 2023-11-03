7c1926168ec547908136dc31a279e1136571c3d2
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-03 08:05:46 -0800

    add seed for root user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // run the seeders
+        $this->call([
+            UserSeeder::class,
+        ]);
     }
 }
