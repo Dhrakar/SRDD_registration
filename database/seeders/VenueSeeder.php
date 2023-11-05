9f4f2f724c3c7e0e524ce3e17b87ac3549278f82
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-05 13:17:18 -0900

    Added Venue table

---
+++ database/seeders/VenueSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class VenueSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::Table('venues')->insert([
+            'location'  => 'To Be Determined...',
+            'max_seats' => -1,
+        ]);
+        DB::Table('venues')->insert([
+            'location'  => 'Online Session',
+            'max_seats' => -1,
+        ]);
+        DB::Table('venues')->insert([
+            'location'  => 'Wood Center Ballroom',
+            'max_seats' => 349,
+        ]);
+        DB::Table('venues')->insert([
+            'location'  => 'Wood Center Mall Area',
+            'max_seats' => 150,
+        ]);
+        DB::Table('venues')->insert([
+            'location'  => 'Wood Center Conf. E/F',
+            'max_seats' => 50,
+        ]);
+        DB::Table('venues')->insert([
+            'location'  => 'Schaible Auditorium',
+            'max_seats' => 250,
+        ]);
+        DB::Table('venues')->insert([
+            'location'  => 'BP Design Theater',
+            'max_seats' => 100,
+        ]);
+    }
+}
