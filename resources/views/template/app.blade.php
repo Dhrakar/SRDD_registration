369d90d94b4857dd11d1018c79c151e9e207a664
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-08 22:10:54 -0900

    add header , footer and nav

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -10,12 +10,17 @@
     <body>
 
         {{-- Banner and navigation blocks --}}
-        <x-global.banner/>
+        <header>
+            <x-global.banner/>
+            <x-global.nav/>
+        </header>
 
         {{-- pull in body content --}}
         <main>
             @yield('content')
         </main>
+
+        {{-- page footer --}}
         
         {{-- Include late-loading items --}}
         <x-global.scripts/>
