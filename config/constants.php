93ba7024c91cd0280d98dc57b0fe5285ee7adccb
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-10 21:35:01 -0900

    new config for srdd constants

---
+++ config/constants.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |---------------------------------------------------------------------------
+    | This file contains the constants and enumerations used by this application
+    |---------------------------------------------------------------------------
+    |
+    | 
+    */
+
+    // date of SRDD
+    'srdd_date' => '10 APR 2024',
+    'srdd_year' => 2024,
+];
