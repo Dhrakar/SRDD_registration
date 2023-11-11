483ade93ff628a3fa07d20b5a6d2b358e2a4f0f6
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-10 21:34:16 -0900

    refactored to use SVG logo

--- resources/views/components/global/banner.blade.php
+++ resources/views/components/global/banner.blade.php
@@ -4,17 +4,15 @@
   -- UAF Yellow: #FFC000
   --}}
 
-<div class="bg-[#236192] flex pt-1 pl-2">
+<div class="bg-[#236192] flex justify-between pt-1 pl-2">
     <div class="flex-none py-2">
         <a href="https://uaf.edu" title="UAF">
-            <img src="https://www.uaf.edu/_resources/images/uaflogoBlue.png" width="80px" height="43px">
+            <x-global.uaf-logo class="block h-16 w-auto fill-current text-white" />
+            <!-- <img src="https://www.uaf.edu/_resources/images/uaflogoBlue.png" width="80px" height="43px"> -->
         </a>
     </div>
-    <div class="flex-none">&nbsp;</div>
-    <div class="flex-initial text-white text-xl font-sans translate-y-4 " >
-        <span class="text-3xl">U</span>NIVERSITY OF 
-        <span class="text-3xl">A</span>LASKA 
-        <span class="text-3xl">F</span>AIRBANKS
+    <div class="inline-flex items-center mr-5 px-1 pt-1 text-md text-white">
+        Staff Recognition &amp; Development Day {{ config('constants.srdd_year') }}
     </div>
 </div>
-<div class="w-screen h-1.5 bg-gradient-to-r from-cyan-200 to-[#FFC000]"></div>
+<div class="w-screen h-1 bg-gradient-to-r from-cyan-200 to-[#FFC000]"></div>
