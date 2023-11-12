f9f61a2fab2d72b8855a12517b7f892597a5b8d3
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-12 10:58:48 -0900

    New files for UI building

---
+++ resources/views/pages/admin/Track/[Track].blade.php
@@ -0,0 +1,13 @@
+@extends('template.app')
+
+@section('content')
+<div>
+    <h3>Tracks...</h3>
+
+    
+      <b>{{ $track->title }}</b></br>
+    
+
+</div>
+@endsection
+
---
+++ resources/views/pages/admin/Track/index.blade.php
@@ -0,0 +1,16 @@
+<?php 
+    /*
+     *  This is the admin landing page for Track models and it shows all of them in a table
+ ``  */
+
+ ?>
+
+@extends('template.app')
+
+@section('content')
+<div>
+    <h3>Tracks...</h3>
+    
+
+</div>
+@endsection
