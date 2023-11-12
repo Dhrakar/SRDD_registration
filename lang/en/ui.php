e0ec48aeddf52bf1516c515c20de690811bc2ad4
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-12 11:24:15 -0900

    add language files  - publish laravel lang files (lang/en)  - create md filder  - copy over .md files from FB

---
+++ lang/en/ui.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Factbook UI Language Text
+    |--------------------------------------------------------------------------
+    |
+    | Language items for various bits of UI -- including tooltips, menus, labels
+    | etc. 
+    */
+
+    'menu' => [
+          'about' => 'About SRDD',
+          'admin' => 'Admin',
+           'dash' => 'Dashboard',
+           'data' => 'Data',
+         'define' => 'Field Definitions',
+           'help' => 'Application Help',
+           'home' => 'Home',
+            'lib' => 'Included Packages',
+          'login' => 'Non-UA Domain Login',
+    ],
+    'markdown' => [
+        // As these are rather longer, they pull in markdown text from files in 
+        // the resources/md folder
+               'about' => file_get_contents(resource_path('md/about.md')),
+         'boilerplate' => file_get_contents(resource_path('md/legal.md')),
+              'gsuite' => file_get_contents(resource_path('md/gsuite_dialog.md')),
+        'introduction' => file_get_contents(resource_path('md/welcome.md')),
+           'libraries' => file_get_contents(resource_path('md/app_packages.md')),
+        
+    ],
+    'auth' => [
+        'uafail' => 'Please click Cancel and use the Sign in With Google widget to login with a UA domain account',
+    ],
+];
---
+++ resources/md/legal.md
@@ -0,0 +1,6 @@
+The University of Alaska Fairbanks is [accredited](https://uaf.edu/accreditation/) by the Northwest Commission on Colleges and Universities.    
+UAF is an AA/EO employer and educational institution and prohibits illegal discrimination against any individual. Learn more about UA’s notice of [nondiscrimination](https://www.alaska.edu/nondiscrimination).    
+UA is committed to providing accessible websites. Learn more about UA’s [notice of web accessibility](https://www.alaska.edu/webaccessibility).    
+[Privacy Statement](https://www.alaska.edu/records/records/compliance/gdpr/ua-privacy-statement/)    
+
+For questions or comments regarding the Staff Recognition &amp; Development Day or this application, please contact [UAF Staff Council](mailto:uaf-staff-council@alaska.edu) | ⓒ UA
