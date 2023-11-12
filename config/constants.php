9e2b22e1328170d03563028ed03ff502a4a5cb56
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-12 10:59:48 -0900

    added color array

--- config/constants.php
+++ config/constants.php
@@ -13,4 +13,18 @@ return [
     // date of SRDD
     'srdd_date' => '10 APR 2024',
     'srdd_year' => 2024,
+
+    // default colors
+    'colors' => [
+         'uaf' => [
+            // primary colors
+            'blue' => '#236192',
+            'yellow' => '#FFC000',      
+         ],
+        'menu' => [
+              'def-text' => 'text-cyan-50',     // unselected text
+              'sel-text' => 'text-yellow-400',  // selected or active menu item text
+        ],
+        'icon' => 'text-slate-200',   // default Icon color
+    ],
 ];
