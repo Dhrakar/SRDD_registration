ab071e026788e46f31a386338258591a3e96cc89
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-12 13:29:59 -0900

    set colors to use constants

--- resources/views/components/global/footer.blade.php
+++ resources/views/components/global/footer.blade.php
@@ -4,12 +4,16 @@
 
 <!--  UAF standard footer -->
 <div class="text-slate-500 text-xs leading-5 mt-16">
-    <div class="mx-auto w-9/12 h-1 bg-slate-800"></div>
+    <div class="mx-auto w-9/12 h-1 bg-sky-900"></div>
     <div class="text-center ">
-      The <a href="http://www.uaf.edu" class="text-blue-500">University of Alaska Fairbanks</a> is an affirmative action/equal 
-      opportunity employer and educational institution and is a part of the 
-      <a href="http://www.alaska.edu" class="text-blue-500">University of Alaska</a> system.<br/>
-      For questions or suggestions about this web application, please contact the  
-      <a href="hhttps://www.uaf.edu/uafgov/staff-council/index.php" class="text-blue-500">UAF Staff Council</a>
+      The <a href="http://www.uaf.edu" class="{{ config('constants.colors.text.link') }}">
+        University of Alaska Fairbanks
+      </a> is an affirmative action/equal opportunity employer and educational institution and is a part 
+      of the <a href="http://www.alaska.edu" class="{{ config('constants.colors.text.link') }}">
+        University of Alaska
+      </a> system.<br/> For questions or suggestions about this web application, please contact the  
+      <a href="hhttps://www.uaf.edu/uafgov/staff-council/index.php" class="{{ config('constants.colors.text.link') }}">
+        UAF Staff Council
+      </a>
     </div>
 </div>
