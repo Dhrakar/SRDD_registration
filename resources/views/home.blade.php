e3df2052d749ae221994065bc6b4070b96b018b4
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-12 15:29:14 -0900

    layout updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,17 @@
+<?php
+    /*
+     *  This is the main landing page for the registration applicaiton
+     */
+
+
+?>
 @extends('template.app')
 
 @section('content')
 
-    <div class="h-screen w-screen border border-slate-200 rounded-sm">
-        <div id="welcome" class="p-5 bg-slate-100 text-sky-900 border-2 border-sky-950 rounded-sm">
-            <span class="text-3xl mx-auto">
-                Welcome to the Staff Recognition and Development Day registration tool
-            </span>
+    <div class="container">
+        <h1> Welcome to the Staff Recognition and Development Day registration tool</h1>
+        <div class="callout">
             <span class="text-justify"> 
                 {!! Str::markdown(__('ui.markdown.welcome')); !!}
             </span>
@@ -14,7 +19,6 @@
         @if (Route::has('login'))
                 <div class="">
                     @auth
-                        <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
