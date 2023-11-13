c5c274be71688040abaa29fe2766f71a91447086
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-12 15:29:38 -0900

    remove specific body classes

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -7,7 +7,7 @@
         {{-- Meta tags, CSS and JS libraries --}}
         <x-global.header/>
     </head>
-    <body class="bg-slate-50 text-base leading-normal {{ config('constants.colors.text.def') }}">
+    <body>
 
         {{-- Banner and navigation blocks --}}
         <header>
