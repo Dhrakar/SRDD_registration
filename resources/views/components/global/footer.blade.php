8493d53563ff7f51665f2e2a296d70d9f6f07c19
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-12 15:05:06 -0900

    fix formatting and link colors

--- resources/views/components/global/footer.blade.php
+++ resources/views/components/global/footer.blade.php
@@ -4,16 +4,12 @@
 
 <!--  UAF standard footer -->
 <div class="text-slate-500 text-xs leading-5 mt-16">
-    <div class="mx-auto w-9/12 h-1 bg-sky-900"></div>
-    <div class="text-center ">
-      The <a href="http://www.uaf.edu" class="{{ config('constants.colors.text.link') }}">
-        University of Alaska Fairbanks
-      </a> is an affirmative action/equal opportunity employer and educational institution and is a part 
-      of the <a href="http://www.alaska.edu" class="{{ config('constants.colors.text.link') }}">
-        University of Alaska
-      </a> system.<br/> For questions or suggestions about this web application, please contact the  
-      <a href="hhttps://www.uaf.edu/uafgov/staff-council/index.php" class="{{ config('constants.colors.text.link') }}">
-        UAF Staff Council
-      </a>
+    <div class="mx-auto w-2/3 h-1 bg-slate-400"></div>
+    <div class="mx-auto w-2/3 p-2 text-center text-xs">
+      The <a href="http://www.uaf.edu" > University of Alaska Fairbanks </a> is an affirmative action/equal 
+      opportunity employer and educational institution and is a part of the 
+      <a href="http://www.alaska.edu" > University of Alaska </a> system. For questions or suggestions 
+      about this web application, please contact the  
+      <a href="hhttps://www.uaf.edu/uafgov/staff-council/index.php" > UAF Staff Council </a>
     </div>
 </div>
