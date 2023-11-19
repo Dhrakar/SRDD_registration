d2e080b43e08dddde2f3809eb98118c8281f03ef
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-18 18:37:47 -0900

    add stubs for schedule and reports

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,23 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('/admin', function () {
-    return view('admin.index');
-})->middleware(['auth', 'verified'])->name('admin.index');
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
 Route::middleware('auth')->group(function () {
+    // --------------------------------
+    //  Main index pages for sections
+    // --------------------------------
+    /* admin */
+    Route::get('/admin', function () { return view('admin.index'); })->name('admin.index');
+    /* session scheduling/viewing */
+    Route::get('/schedule', function () { return view('schedule.index'); })->name('schedule.index');
+    /* Reports */
+    Route::get('/reports', function () { return view('reports.index'); })->name('reports.index');
+
+    /* user profile */
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
