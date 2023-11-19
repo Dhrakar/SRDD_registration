b56be0ad5cdde8c9de42ad3e7ea87be7b8eb12e9
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-18 15:15:12 -0900

    new component for info cards

---
+++ resources/views/components/srdd/callout.blade.php
@@ -0,0 +1,5 @@
+<div class="ml-8 mr-3 mt-2 mb-10 px-2 py-4 rounded-sm border-l-8 border-sky-700 bg-sky-50 shadow-md text-sm">
+    <span class="text-justify">
+        {{ $slot }}
+    </span>
+</div>
