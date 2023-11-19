56b3f81f7db330b87c878f371fb5ed313446962c
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-19 10:34:46 -0900

    update the width

--- resources/views/admin/tracks/index.blade.php
+++ resources/views/admin/tracks/index.blade.php
@@ -11,7 +11,7 @@
 
 @section('content')
 <x-admin.nav-admin/>
-<div class="container">
+<div class="container w-full">
   <x-srdd.callout>
     {!! Str::markdown(__('ui.markdown.intro-tracks')); !!}
   </x-srdd.callout>
@@ -83,7 +83,7 @@
       </div>
 
   {{-- List existing tracks --}}
-  <div class="mx-10 mt-4 pb-5 w-full border border-indigo-900 rounded-md">
+  <div class="mx-10 mt-4 pb-5 w-auto border border-indigo-900 rounded-md">
       <div class="ml-10 px-2 -translate-y-3 w-min bg-white font-bold">
       Currently&nbsp;Configured&nbsp;Tracks
       </div> 
