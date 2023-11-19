48e3e6eb318070fc877f4fe23b915742c4a14012
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-18 19:09:33 -0900

    new landing pages

---
+++ resources/views/reports/index.blade.php
@@ -0,0 +1,17 @@
+<?php 
+    /*
+     *  Main SRDD reporting page
+     */
+
+ ?>
+@extends('template.app')
+
+@section('content')
+<div class="container">
+    <div class="mx-10 mt-4 pb-5 w-auto border border-indigo-900 rounded-md">
+        <div class="ml-10 px-2 -translate-y-3 w-min bg-white font-bold">
+        Reports
+        </div> 
+    </div>
+</div>
+@endsection
