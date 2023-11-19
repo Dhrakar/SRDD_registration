74d54de6ebfc94babf058975bd1d0dab727400b9
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-18 18:32:30 -0900

    remove old login link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,12 +14,14 @@
         <x-srdd.callout>
             {!! Str::markdown(__('ui.markdown.welcome')); !!}
         </x-srdd.callout>
+        {{-- Add in the Login and Registration forms --}}
+        <x-srdd.login-std/>
+        
         @if (Route::has('login'))
                 <div class="">
                     @auth
                     @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-
+                        
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
                         @endif
