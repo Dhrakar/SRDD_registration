f5668c697432180f9315ce37070180a6b1791b6b
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-21 11:01:56 -0900

    new widget for switching theme

---
+++ resources/views/components/global/theme-sel.blade.php
@@ -0,0 +1,12 @@
+<div class="dropdown">
+    <div class="tooltip" data-tip="Change Theme">
+        <label tabindex="0" class="mt-2 btn text-xs bg-slate-600 text-[#FFC000]">
+            <i class="bi bi-moon-stars-fill"></i>
+            <svg width="12px" height="12px" class="h-2 w-2 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg>
+        </label>
+    </div>
+  <ul tabindex="0" class="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
+    <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-xs btn-block btn-ghost justify-start" aria-label="Light" value="light"/></li>
+    <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-xs btn-block btn-ghost justify-start" aria-label="Dark" value="dark"/></li>
+  </ul>
+</div>
