de3c52eff5e9eb1e668391fb5d8a44f36f43e08e
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-21 17:50:17 -0900

    update for dark and add divider

--- resources/views/components/global/footer.blade.php
+++ resources/views/components/global/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="text-slate-700 dark:text-slate-50 text-xs leading-5 mt-16">
-    <div class="divider dark:divider-info"></div>
+    <div class=" bg-slate-500 dark:bg-sky-400 h-1 w-3/4 mx-auto"></div>
     <div class="mx-auto w-2/3 p-2 text-center text-xs">
       The <a href="http://www.uaf.edu" > University of Alaska Fairbanks </a> is an affirmative action/equal 
       opportunity employer and educational institution and is a part of the 
