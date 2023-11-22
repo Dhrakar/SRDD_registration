a983e55621d5378490b9b639322212028cae75f7
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-21 15:58:40 -0900

    added dark mode colors

--- resources/views/components/srdd/callout.blade.php
+++ resources/views/components/srdd/callout.blade.php
@@ -1,5 +1,5 @@
-<div class="ml-8 mr-3 mt-2 mb-10 px-2 py-4 rounded-sm border-l-8 border-sky-700 bg-sky-50 shadow-md text-sm">
-    <span class="text-justify">
+<div class="ml-8 mr-3 mt-2 mb-10 px-2 py-4 rounded-md border-2 border-l-8 border-sky-800 dark:border-sky-100 bg-sky-50 dark:bg-sky-500 shadow-md">
+    <span class="text-justify text-sm text-slate-700 dark:text-slate-50">
         {{ $slot }}
     </span>
 </div>
