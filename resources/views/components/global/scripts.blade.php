c58e476c1c08b40a0f51bb134f02f5c94284070b
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-21 17:42:57 -0900

    remove theme switcher

--- resources/views/components/global/scripts.blade.php
+++ resources/views/components/global/scripts.blade.php
@@ -1,9 +1,6 @@
 {{-- Late loading scripts, dialogs and other items --}}
 
 <div>
-    <!-- load in the theme switcher library -->
-    <script src="https://cdn.jsdelivr.net/npm/theme-change@2.0.2/index.js"></script>
-    
     <!-- load in the Google Auth & JWT libraries -->
     <script src="https://accounts.google.com/gsi/client" async defer></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jsrsasign/8.0.20/jsrsasign-all-min.js"></script>
