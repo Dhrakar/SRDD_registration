fcf9ef3150b73a90e1e214e72df536ab9100505f
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-22 17:39:17 -0900

    reduce top margin

--- resources/views/components/global/footer.blade.php
+++ resources/views/components/global/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="text-slate-700 dark:text-slate-50 text-xs leading-5 mt-16">
+<div class="text-slate-700 dark:text-slate-50 text-xs leading-5 mt-4">
     <div class=" bg-slate-500 dark:bg-sky-400 h-0.5 w-3/4 mx-auto"></div>
     <div class="mx-auto w-2/3 p-2 text-center text-xs">
       The <a class="underline" href="http://www.uaf.edu" > University of Alaska Fairbanks </a> is an affirmative action/equal 
