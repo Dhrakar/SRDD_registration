d99c868de2fa59b180cba0c0bdbb385e6178bccc
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-22 21:32:58 -0900

    move boilerplate to About

--- resources/views/components/global/footer.blade.php
+++ resources/views/components/global/footer.blade.php
@@ -1,10 +1,3 @@
 <div class="text-slate-700 dark:text-slate-50 text-xs leading-5 mt-4">
-    <div class=" bg-slate-500 dark:bg-sky-400 h-0.5 w-3/4 mx-auto"></div>
-    <div class="mx-auto w-2/3 p-2 text-center text-xs">
-      The <a class="underline" href="http://www.uaf.edu" > University of Alaska Fairbanks </a> is an affirmative action/equal 
-      opportunity employer and educational institution and is a part of the 
-      <a class="underline" href="http://www.alaska.edu" > University of Alaska </a> system. For questions or suggestions 
-      about this web application, please contact the  
-      <a class="underline" href="https://www.uaf.edu/uafgov/staff-council/index.php" > UAF Staff Council </a>
-    </div>
+
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,14 +11,30 @@
     <div class="container">
         <x-global.nav-header />
         <x-srdd.callout :title="__('Welcome to the Staff Recognition and Development Day registration tool!')" >
-            {!! Str::markdown(__('ui.markdown.welcome')); !!}
+            <p>
+                This registration tool will allow you to register for sessions presented by your fellow UAF 
+                employees covering many diverse topics.  The main sessions will be presented on the 
+                <a class="dark:text-amber-400 underline" href="https://media.uaf.edu/home" target="_blank">UAF Media Stream</a> server, while the 
+                remaining fun/development sessions may be presented via individual Zoom Conference. Please 
+                be sure to sign up for the sessions you are interested in so that we can send you any Zoom 
+                informaiton for these sessions.  Registration will also help us plan for future Staff 
+                Recognition days.
+            </p>
         </x-srdd.callout>
         
-        {{-- Add in the Login form if needed --}}
-        @guest 
-        <x-srdd.login/>
-        @endguest
-        
+        {{-- Add in the Login form or logged in user info --}}
+        <x-global.title-box :title="__('Login')">
+            @guest 
+                <x-srdd.login/>
+            @endguest
+            @auth
+                <div class="bg-emerald-200 dark:bg-emerald-800 border border-emerald-900 dark:border-emerald-100 rounded-md p-4 mx-4 text-std">
+                    <i class="bi bi-info-circle"></i><span class="pl-4 font-semibold">Logged In</span>
+                    <x-global.divider/>
+                    Welcome back {{ Auth::user()->name }}! You last logged in on {{ Auth::user()->last_login }}.
+                </div>
+            @endauth
+        </x-global.title-box>
         
     </div>
 
