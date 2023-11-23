23e728e2f0803b1a0a9b6a9ac168debf58dbe700
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-22 21:08:31 -0900

    switch default icon

--- resources/views/components/global/nav-header.blade.php
+++ resources/views/components/global/nav-header.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-slate-700 text-sm font-bold py-1 w-screen" style="list-style-type: none;">
     <ui class="flex">
         <li class="mx-2 text-indigo-300">
-            <i class="bi bi-gear-wide-connected"></i>
+            <i class="bi bi-pc-display-horizontal"></i>
         </li>
 
         {{-- Remaining list items for menu --}}
