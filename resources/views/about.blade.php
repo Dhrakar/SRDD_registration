bec49f8a8e4214eec7b737acb90a7b9ed0680258
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-22 16:38:46 -0900

    switch to using callout and box comp.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -20,9 +20,7 @@
 
 @section('content')
 <div class="container w-full">
-    <h1>About This Application</h1>
-    <x-srdd.callout>
-        
+    <x-srdd.callout :title="__('SRDD Registration Tool')">
         <em>Staff Recognition &amp; Development Day</em> is a time for us to recognize and thank the hard work done by UAF staff throughout the year.
 	    It is also an opportunity to celebrate some longevity milestones for our staff.  Lastly, it is a day for staff to be able
 	    to participate in fun training and informational sessions that can help in their personal and professional development.
@@ -36,10 +34,7 @@
     </x-srdd.callout>
 
     {{-- List out the libraries and licenses used --}}
-    <div class="mx-10 mt-4 pb-5 w-auto border border-indigo-900 rounded-md">
-        <div class="ml-10 px-2 -translate-y-3 w-min bg-white font-bold">
-            Libraries&nbsp;and&nbsp;Licenses
-        </div> 
+    <x-global.title-box :title="__('Libraries and Licenses')">
         <p class="px-8 text-sm italic ">Note that there are many other packeges included in the Laravel framework.  This table only lists the major
             packages that have been installed beyond laravel.
         </p>
@@ -74,19 +69,19 @@
             <div class="px-2 table-row col-span-2"></div>
             <div class="px-2 table-row col-span-2"></div>
 
-<div class="px-2 table-row col-span-3"></div>
-<div class="px-2 table-row col-span-1"></div>
-<div class="px-2 table-row col-span-4"></div>
-<div class="px-2 table-row col-span-2"></div>
-<div class="px-2 table-row col-span-2"></div>
+            <div class="px-2 table-row col-span-3"></div>
+            <div class="px-2 table-row col-span-1"></div>
+            <div class="px-2 table-row col-span-4"></div>
+            <div class="px-2 table-row col-span-2"></div>
+            <div class="px-2 table-row col-span-2"></div>
 
-<div class="px-2 table-row col-span-3"></div>
-<div class="px-2 table-row col-span-1"></div>
-<div class="px-2 table-row col-span-4"></div>
-<div class="px-2 table-row col-span-2"></div>
-<div class="px-2 table-row col-span-2"></div>
+            <div class="px-2 table-row col-span-3"></div>
+            <div class="px-2 table-row col-span-1"></div>
+            <div class="px-2 table-row col-span-4"></div>
+            <div class="px-2 table-row col-span-2"></div>
+            <div class="px-2 table-row col-span-2"></div>
         </div>
-    </div>
+    </x-global.title-box>
 </div>
 
 @endsection
