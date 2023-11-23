43fc5232a7a1580383b59be216cb5a5ac03c314d
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-23 10:03:20 -0900

    add flatpickr

--- resources/views/components/global/header.blade.php
+++ resources/views/components/global/header.blade.php
@@ -10,6 +10,9 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;600;700&display=swap">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 
+<!-- CSS for flatpickr --> 
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+
 <!-- compiled css and js -->
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 
