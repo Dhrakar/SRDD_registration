0436a90f204512a776e8f135ba8320f7d5ac7d44
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-22 20:34:43 -0900

    new widget for sub-nav

---
+++ resources/views/components/global/nav-header.blade.php
@@ -0,0 +1,10 @@
+<div class="bg-slate-700 text-sm font-bold py-1 w-screen" style="list-style-type: none;">
+    <ui class="flex">
+        <li class="mx-2 text-indigo-300">
+            <i class="bi bi-gear-wide-connected"></i>
+        </li>
+
+        {{-- Remaining list items for menu --}}
+        {{ $slot }}
+    </ul>
+</div>
