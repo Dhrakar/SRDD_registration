0ad4a3f0ab0c58605f6443a19a7cd3f569d49552
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-23 10:39:48 -0900

    add slots and update routes

--- resources/views/components/admin/nav-admin.blade.php
+++ resources/views/components/admin/nav-admin.blade.php
@@ -17,23 +17,23 @@
                 href="{{ route('tracks.index') }}">{{__('Tracks')}}</a>
         </li>
         <li class="mr-6">
-            <a  class="{{ (url()->current() == route('home'))?$m_text_sel:$m_text_def }}" 
+            <a  class="{{ (strpos(url()->current(), 'venues') !== false )?$m_text_sel:$m_text_def }}" 
                 href="#">{{__('Venues')}}</a>
         </li>
         <li class="mr-6">
-            <a  class="{{ (url()->current() == route('home'))?$m_text_sel:$m_text_def }}" 
-                href="#">{{__('Slots')}}</a>
+            <a  class="{{ (strpos(url()->current(), 'slots') !== false )?$m_text_sel:$m_text_def }}" 
+                href="{{ route('slots.index') }}">{{__('Slots')}}</a>
         </li>
         <li class="mr-6">
-            <a  class="{{ (url()->current() == route('home'))?$m_text_sel:$m_text_def }}" 
+            <a  class="{{ (strpos(url()->current(), 'users') !== false )?$m_text_sel:$m_text_def }}" 
                 href="#">{{__('Users')}}</a>
         </li>
         <li class="mr-6">
-            <a  class="{{ (url()->current() == route('home'))?$m_text_sel:$m_text_def }}" 
+            <a  class="{{ (strpos(url()->current(), 'events') !== false )?$m_text_sel:$m_text_def }}" 
                 href="#">{{__('Events')}}</a>
         </li>
         <li class="mr-6">
-            <a  class="{{ (url()->current() == route('home'))?$m_text_sel:$m_text_def }}" 
+            <a  class="{{ (strpos(url()->current(), 'sessions') !== false )?$m_text_sel:$m_text_def }}" 
                 href="#">{{__('Sessions')}}</a>
         </li>
 </x-global.nav-header>
