9f86929a8a2ba80f90c70f9834fc574beb64a34f
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-22 20:49:54 -0900

    update to use widgets

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -16,10 +16,8 @@
 @section('content')
 <x-admin.nav-admin/>
 <div class="container">
-    <div class="mx-10 mt-4 pb-5 w-auto border border-indigo-900 rounded-md">
-        <div class="ml-10 px-2 -translate-y-3 w-min bg-white font-bold">
-        Configuration&nbsp;&amp;&nbsp;Admin
-        </div> 
-    </div>
+    <x-global.title-box :title="__('Admin/Configuration')">
+        This is where you do stuff
+    </x-global.title-box>
 </div>
 @endsection
--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -8,10 +8,7 @@
 
 @section('content')
 <div class="container">
-    <div class="mx-10 mt-4 pb-5 w-auto border border-indigo-900 rounded-md">
-        <div class="ml-10 px-2 -translate-y-3 w-min bg-white font-bold">
-        Reports
-        </div> 
-    </div>
+    <x-global.nav-header />
+    <x-global.title-box :title="__('Reports')"/>
 </div>
 @endsection
