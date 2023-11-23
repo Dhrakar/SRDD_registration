dd2fc20924ca3593cc508abb48ea8a57a5544620
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-22 16:37:35 -0900

    underlines, dark mode

--- resources/views/components/global/footer.blade.php
+++ resources/views/components/global/footer.blade.php
@@ -1,10 +1,10 @@
 <div class="text-slate-700 dark:text-slate-50 text-xs leading-5 mt-16">
-    <div class=" bg-slate-500 dark:bg-sky-400 h-1 w-3/4 mx-auto"></div>
+    <div class=" bg-slate-500 dark:bg-sky-400 h-0.5 w-3/4 mx-auto"></div>
     <div class="mx-auto w-2/3 p-2 text-center text-xs">
-      The <a href="http://www.uaf.edu" > University of Alaska Fairbanks </a> is an affirmative action/equal 
+      The <a class="underline" href="http://www.uaf.edu" > University of Alaska Fairbanks </a> is an affirmative action/equal 
       opportunity employer and educational institution and is a part of the 
-      <a href="http://www.alaska.edu" > University of Alaska </a> system. For questions or suggestions 
+      <a class="underline" href="http://www.alaska.edu" > University of Alaska </a> system. For questions or suggestions 
       about this web application, please contact the  
-      <a href="https://www.uaf.edu/uafgov/staff-council/index.php" > UAF Staff Council </a>
+      <a class="underline" href="https://www.uaf.edu/uafgov/staff-council/index.php" > UAF Staff Council </a>
     </div>
 </div>
