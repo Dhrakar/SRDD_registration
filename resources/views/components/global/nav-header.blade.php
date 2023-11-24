765f6beac475456095e2b0a038adfdb0a7d0dfce
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-24 05:52:02 -0900

    add link for icon

--- resources/views/components/global/nav-header.blade.php
+++ resources/views/components/global/nav-header.blade.php
@@ -1,7 +1,18 @@
+{{--
+  -- Creates the navigation sub-tabs.  Optional href for the icon
+  --}}
+
+
+@props(['target' => '#'])
+
 <div class="bg-slate-700 text-sm font-bold py-1 w-screen" style="list-style-type: none;">
     <ui class="flex">
         <li class="mx-2 text-indigo-300">
-            <i class="bi bi-pc-display-horizontal"></i>
+            <a class=""
+                href="{{ $target }}"
+            >
+                <i class="bi bi-pc-display-horizontal"></i>
+            </a>
         </li>
 
         {{-- Remaining list items for menu --}}
