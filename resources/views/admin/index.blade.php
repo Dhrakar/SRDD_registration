f2e1ca0fe3a36ea5ddb0afe377205fa7c326b04f
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-24 05:50:59 -0900

    add callout text

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -16,8 +16,14 @@
 @section('content')
 <x-global.nav-admin/>
 <div class="container">
-    <x-srdd.title-box :title="__('Admin/Configuration')">
-        This is where you do stuff
+    <x-srdd.callout :title="__('Admin and Configuration')">
+        These pages are where you can update, add or delete the various components of Staff Recognition Day.
+        Note that you will not be able to edit or delete any Track, Venue or Slot or User with ID #1
+    </x-srdd.calloutx>
+    <x-srdd.title-box :title="__('Current Application Settings')">
+        <div class="p-4 text-std">
+        Event Date: {{config('constants.srdd_date')}}
+        </div>
     </x-srdd.title-box>
 </div>
 @endsection
