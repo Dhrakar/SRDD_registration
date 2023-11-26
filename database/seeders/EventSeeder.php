dafdec919eb0596efd105355602f1fd929d690e2
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-25 17:17:08 -0900

    fix errors - note , must be run in right order since events, sessions and schedules build on each other

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EventSeeder extends Seeder
 {
@@ -14,54 +15,54 @@ class EventSeeder extends Seeder
     {
         // add a couple of default events
         DB::Table('events')->insert([
-            'track_id' => 1,
-            'user_id' => 0,
-            'year' => config('constants.srdd_year'),
-            'title' => 'Longevity Awards',
+            'track_id'    => 1,
+            'user_id'     => 0,
+            'year'        => config('constants.srdd_year'),
+            'title'       => 'Longevity Awards',
             'description' => 'Staff recognition for UA employment milestones: 5,10,15,20,25,30,35,40,45 and 50 years.',
-            'need_reg' => false,
-            'created_at'        => now(),
-            'updated_at'        => now(),
+            'needs_reg'   => false,
+            'created_at'  => now(),
+            'updated_at'  => now(),
         ]);
         DB::Table('events')->insert([
-            'track_id' => 1,
-            'user_id' => 0,
-            'year' => config('constants.srdd_year'),
-            'title' => "Chancellor's Presentation",
+            'track_id'    => 1,
+            'user_id'     => 0,
+            'year'        => config('constants.srdd_year'),
+            'title'       => "Chancellor's Presentation",
             'description' => 'Presentation from the Chancellor + Cornerstone and other special awards.',
-            'need_reg' => false,
-            'created_at'        => now(),
-            'updated_at'        => now(),
+            'needs_reg'   => false,
+            'created_at'  => now(),
+            'updated_at'  => now(),
         ]);
         DB::Table('events')->insert([
-            'track_id' => 1,
-            'user_id' => 0,
-            'year' => config('constants.srdd_year'),
-            'title' => 'Lunch',
+            'track_id'    => 1,
+            'user_id'     => 0,
+            'year'        => config('constants.srdd_year'),
+            'title'       => 'Lunch',
             'description' => 'Meal provided for the attendees to enjoy while gathering together.',
-            'need_reg' => true,
-            'created_at'        => now(),
-            'updated_at'        => now(),
+            'needs_reg'   => true,
+            'created_at'  => now(),
+            'updated_at'  => now(),
         ]);
         DB::Table('events')->insert([
-            'track_id' => 1,
-            'user_id' => 0,
-            'year' => config('constants.srdd_year'),
-            'title' => 'Coffee, Tea and Goodies',
+            'track_id'    => 1,
+            'user_id'     => 0,
+            'year'        => config('constants.srdd_year'),
+            'title'       => 'Coffee, Tea and Goodies',
             'description' => 'Morning meal provided for the attendees to enjoy while gathering together.',
-            'need_reg' => true,
-            'created_at'        => now(),
-            'updated_at'        => now(),
+            'needs_reg'   => true,
+            'created_at'  => now(),
+            'updated_at'  => now(),
         ]);
         DB::Table('events')->insert([
-            'track_id' => 1,
-            'user_id' => 0,
-            'year' => config('constants.srdd_year'),
-            'title' => 'Keynote',
+            'track_id'    => 1,
+            'user_id'     => 0,
+            'year'        => config('constants.srdd_year'),
+            'title'       => 'Keynote',
             'description' => 'Presentation to UAF staff by an invited speaker.',
-            'need_reg' => false,
-            'created_at'        => now(),
-            'updated_at'        => now(),
+            'needs_reg'   => false,
+            'created_at'  => now(),
+            'updated_at'  => now(),
         ]);
     }
 }
