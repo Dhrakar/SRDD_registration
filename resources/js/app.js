b53602e5418894fbbd88c38a6af9e5e2364b5e60
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-11-28 17:39:25 -0900

    init the fullcalendar libray

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,11 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import { Calendar } from '@fullcalendar/core';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+
+window.Calendar = Calendar;
+window.timeGridPlugin = timeGridPlugin;
+window.listPlugin = listPlugin;
