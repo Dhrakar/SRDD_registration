dcbdf5f6a3b1c547eabd7f447a6ee04af01bc15e
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-12-02 13:53:10 -0900

    add new helpers file - has ordinal function - added to autoloader

---
+++ app/helpers.php
@@ -0,0 +1,16 @@
+<?php
+    /**
+     * Custom helper functions 
+     */
+
+    // emits the ordinal version of a number
+    if(! function_exists('ordinal')) {
+        function ordinal($number) {
+            $ends = array('th','st','nd','rd','th','th','th','th','th','th');
+            if ((($number % 100) >= 11) && (($number%100) <= 13))
+                return $number. 'th';
+            else
+                return $number. $ends[$number % 10];
+        }
+    }
+?>
