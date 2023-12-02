7a0004914cf88ecf2f7ad8b38f01c254570bf8b8
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-12-02 11:33:26 -0900

    add calendar index

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// main landing page (with or without login )
 Route::get('/', function () {
     return view('home');
 })->name('home');
+Route::get('/home', function () {
+    return view('home');
+});
 
+// about this app page
 Route::get('/about', function () {
     return view('about');
 })->name('about');
 
+// just in case is needed to fix an auth issue
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -37,7 +43,7 @@ Route::get('/dashboard', function () {
 // route for logging in via the UA domain
 Route::post('/ualogin', [UALoginController::class, 'uaLogin'])->name('ualogin');
 
-
+// authenticated pages
 Route::middleware('auth')->group(function () {
     // --------------------------------
     //  Main index pages for sections
@@ -46,47 +52,53 @@ Route::middleware('auth')->group(function () {
     Route::get('/admin', function () { return view('admin.index'); })->name('admin.index');
 
     /* main calendar */
+    // index
     Route::get('/calendar', CalendarController::class)->name('calendar');
+    // add sessions
+    // delete sessions
+    // get schedule
 
     /* Reports */
     Route::get('/reports', function () { return view('reports.index'); })->name('reports.index');
 
-    /* config pages */
+    /** 
+     * Admin config pages 
+     */
+
+    // User profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    // 
+    // session tracks
     Route::get('/admin/tracks', [TrackController::class, 'index'])->name('tracks.index');
     Route::post('/admin/tracks/store', [TrackController::class, 'store'])->name('tracks.store');
     Route::get('/admin/tracks/{track}/edit', [TrackController::class, 'edit'])->name('tracks.edit');
     Route::patch('/admin/tracks/{track}', [TrackController::class, 'update'])->name('tracks.update');
     Route::delete('/admin/tracks/{track}/destroy', [TrackController::class, 'destroy'])->name('tracks.destroy');
-    // 
+    // session slots
     Route::get('/admin/slots', [SlotController::class, 'index'])->name('slots.index');
     Route::post('/admin/slots/store', [SlotController::class, 'store'])->name('slots.store');
     Route::get('/admin/slots/{slot}/edit', [SlotController::class, 'edit'])->name('slots.edit');
     Route::patch('/admin/slots/{slot}', [SlotController::class, 'update'])->name('slots.update');
     Route::delete('/admin/slots/{slot}/destroy', [SlotController::class, 'destroy'])->name('slots.destroy');
-    // 
+    // session venues
     Route::get('/admin/venues', [VenueController::class, 'index'])->name('venues.index');
     Route::post('/admin/venues/store', [VenueController::class, 'store'])->name('venues.store');
     Route::get('/admin/venues/{venue}/edit', [VenueController::class, 'edit'])->name('venues.edit');
     Route::patch('/admin/venues/{venue}', [VenueController::class, 'update'])->name('venues.update');
     Route::delete('/admin/venues/{venue}/destroy', [VenueController::class, 'destroy'])->name('venues.destroy');
-    // 
+    // session events
     Route::get('/admin/events', [EventController::class, 'index'])->name('events.index');
     Route::post('/admin/events/store', [EventController::class, 'store'])->name('events.store');
     Route::get('/admin/events/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
     Route::patch('/admin/events/{event}', [EventController::class, 'update'])->name('events.update');
     Route::delete('/admin/events/{event}/destroy', [EventController::class, 'destroy'])->name('events.destroy');
-    // 
+    // sessions
     Route::get('/admin/sessions', [SessionController::class, 'index'])->name('sessions.index');
     Route::post('/admin/sessions/store', [SessionController::class, 'store'])->name('sessions.store');
     Route::get('/admin/sessions/{session}/edit', [SessionController::class, 'edit'])->name('sessions.edit');
     Route::patch('/admin/sessions/{session}', [SessionController::class, 'update'])->name('sessions.update');
     Route::delete('/admin/sessions/{session}/destroy', [SessionController::class, 'destroy'])->name('sessions.destroy');
-    //
-    
 });
 
 
