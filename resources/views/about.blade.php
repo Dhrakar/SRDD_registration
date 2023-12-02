12ee2e0435a2c4dbcc3815b7fc77e02bfa3cfcba
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-12-02 11:23:53 -0900

    updated  - moved boilerplate to notice section   - reformat boilerplate  - added fullcalendaar

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,6 +9,9 @@
     // current tailwinds version
     $tw_ver = json_decode(file_get_contents( base_path('node_modules/tailwindcss/package.json')), true );
 
+    // current fullcalendar version
+    $fc_ver = json_decode(file_get_contents( base_path('node_modules/@fullcalendar/core/package.json')), true );
+
     //current git commit info
     $git = rtrim('Latest git hash: ' . shell_exec("git log --oneline -1 | cut -f1 -d' '")); 
 
@@ -20,7 +23,6 @@
 
 @section('content')
 <div class="container w-full">
-    <x-global.nav-header />
     <x-srdd.callout :title="__('SRDD Registration Tool')">
         <em>Staff Recognition &amp; Development Day</em> is a time for us to recognize and thank the hard work done by UAF staff throughout the year.
 	    It is also an opportunity to celebrate some longevity milestones for our staff.  Lastly, it is a day for staff to be able
@@ -32,16 +34,17 @@
 	    <div class="ml-8 text-md">Registration</div>
 	    <div class="ml-8 text-md">Reports</div>
 	    <div class="ml-8 text-md">Administration</div>
-        <x-srdd.divider/>
-        <div class="mx-auto w-4/5 p-2 text-center text-xs">
+    </div>
+    </x-srdd.callout>
+    <x-srdd.notice :title="__('')">
+        <div class="mx-auto w-9/10 p-2 text-center text-sm">
             The <a class="dark:text-amber-400 underline" href="http://www.uaf.edu" > University of Alaska Fairbanks </a> is an affirmative action/equal 
             opportunity employer and educational institution and is a part of the 
             <a class="dark:text-amber-400 underline" href="http://www.alaska.edu" > University of Alaska </a> system. For questions or suggestions 
             about this web application, please contact the  
             <a class="dark:text-amber-400 underline" href="https://www.uaf.edu/uafgov/staff-council/index.php" > UAF Staff Council </a>
         </div>
-    </div>
-    </x-srdd.callout>
+    </x-srdd.notice>
 
     {{-- List out the libraries and licenses used --}}
     <x-srdd.title-box :title="__('Libraries and Licenses')">
@@ -74,11 +77,11 @@
             <div class="px-2 table-row col-span-2">{!! $lic_mit !!}</div>
             <div class="px-2 table-row col-span-2"><a href="https://tailwindcss.com/" target="_blank">Tailwinds CSS</a></div>
             
-            <div class="px-2 table-row col-span-3">Google Auth API</div>
-            <div class="px-2 table-row col-span-1"></div>
-            <div class="px-2 table-row col-span-4">Javascript components for UA Google SSO</div>
-            <div class="px-2 table-row col-span-2"></div>
-            <div class="px-2 table-row col-span-2"></div>
+            <div class="px-2 table-row col-span-3">FullCalendar</div>
+            <div class="px-2 table-row col-span-1">{{ $fc_ver['version'] }}</div>
+            <div class="px-2 table-row col-span-4">Javascript based calendar for sessions and schedules</div>
+            <div class="px-2 table-row col-span-2">{!! $lic_mit !!}</div>
+            <div class="px-2 table-row col-span-2"><a href="https://fullcalendar.io" target="_blank">FullCalendar JS</a></div>
 
             <div class="px-2 table-row col-span-3">Flatpickr</div>
             <div class="px-2 table-row col-span-1">v4</div>
