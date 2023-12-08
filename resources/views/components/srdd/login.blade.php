0f8e104b3140b33aea03760fabb926738d902fb9
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-12-08 12:59:01 -0900

    move error msg to out of civ

--- resources/views/components/srdd/login.blade.php
+++ resources/views/components/srdd/login.blade.php
@@ -21,17 +21,16 @@
       --}}
     <form name="std_login" method="POST" action="{{ route('login') }}">
         @csrf
-
+        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <x-input-error :messages="$errors->get('password')" class="mt-2" />
         <div class="mx-2 grid grid-cols-6 auto-col-max-6 gap-1">
             {{-- email address --}}
             <x-srdd.form-label class="col-span-1" for="email" :value="__('Email')" />
             <x-srdd.form-txt-input class="col-span-4" id="email" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
             <div class="col-span-1">&nbsp;</div>
             {{-- password --}}
             <x-srdd.form-label class="col-span-1" for="password" :value="__('Password')" />
             <x-srdd.form-txt-input class="col-span-4" type="password" name="password" required autocomplete="current-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
             <div class="col-span-1">&nbsp;</div>
             {{-- Store in session --}}
             <div class="col-span-1">&nbsp;</div>
@@ -58,27 +57,28 @@
 
 
 <!-- script to process the UA auth -->
-
-<script>
-    function handleCredentialResponse(response) {
-      // get the JWT credential blob
-      var myCred = response.credential;
-      // get the payload object
-      var credPayload = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(myCred.split(".")[1]));
-        // now that we have a google login, append hidden data to form and also turn on the submit button
-        $('#gs_login_email').val(credPayload.email);
-        $('#gs_login_id').val(credPayload.name);
-        $('#gs_login_token').val(myCred);
-        $("#gs_login_submit").removeAttr("disabled");
-    }
-    window.onload = function () {
-      google.accounts.id.initialize({
-        client_id: "{{ env('UA_CLIENT_ID') }}",
-        callback: handleCredentialResponse
-      });
-      google.accounts.id.renderButton(
-        document.getElementById("buttonDiv"),
-        { theme: "outline", size: "large" }  // customization attributes
-      );
-    }
-</script>
+{{--
+-- <script>
+--     function handleCredentialResponse(response) {
+--       // get the JWT credential blob
+--       var myCred = response.credential;
+--       // get the payload object
+--       var credPayload = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(myCred.split(".")[1]));
+--         // now that we have a google login, append hidden data to form and also turn on the submit button
+--         $('#gs_login_email').val(credPayload.email);
+--         $('#gs_login_id').val(credPayload.name);
+--         $('#gs_login_token').val(myCred);
+--         $("#gs_login_submit").removeAttr("disabled");
+--     }
+--     window.onload = function () {
+--       google.accounts.id.initialize({
+--         client_id: "{{ env('UA_CLIENT_ID') }}",
+--         callback: handleCredentialResponse
+--       });
+--       google.accounts.id.renderButton(
+--         document.getElementById("buttonDiv"),
+--         { theme: "outline", size: "large" }  // customization attributes
+--       );
+--     }
+-- </script>
+--}}
