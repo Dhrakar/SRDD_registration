89c243c1e2162bc88fe0afd49600c3c241e52914
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-12-12 13:55:44 -0900

    Decided that the 'raw' package json files _do_need to by sync'd - edit to Print page for buttons

--- resources/views/schedule/attendee/print.blade.php
+++ resources/views/schedule/attendee/print.blade.php
@@ -1,80 +1,79 @@
 <?php
     /**
-     * User calendar
+     * Print layout for the user's schedule.  It does not rely on the global header/footer so
+     * that it an more easily be printed without the headers, etc.
      * 
-     * @param $session 
+     * @param $user
      */
- 
-    use App\Models\Schedule;
-    use App\Models\User;
-
+    use Illuminate\Support\Carbon;
+    use Illuminate\Support\Facades\Auth;
     // current user
     $user = Auth::user();
     // format the srdd date for use with teh DATE field
     $srdd_date = config('constants.db_srdd_date'); 
+    // reformat the SRDD date for the header
+    $_date = Carbon::parse(env('SRD_DAY', now()))->toFormattedDateString();
 ?>
-@extends('template.app')
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>{{ config('app.name', 'Application') }}</title>
+            {{-- Meta tags, CSS and JS libraries --}}
+            <x-global.header/>
+        </head>
+        <main>
+            <div class="container">
+                <a  class="px-4 py-2 bg-sky-500 border border-sky-950 rounded-sm 
+                           font-semibold text-xs text-gray-700 uppercase tracking-widest
+                           shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 
+                           focus:ring-indigo-500 focus:ring-offset-2 print:hidden"
+                    href="{{route('schedule')}}">Return to Schedule</a>
+                {{-- check for any sessions for this user --}}
+                @if ($user->schedules->count() == 0) 
+                <span class="text-sm">
+                    You do not have any events scheduled </br>
+                </span>
+                @else
+                <span>
+                    <input class="px-4 py-2 bg-emerald-500 border border-emerald-950 rounded-sm 
+                           font-semibold text-xs text-gray-700 uppercase tracking-widest
+                           shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 
+                           focus:ring-indigo-500 focus:ring-offset-2 print:hidden"
+                           id="printme" type="button" value="Print this page" 
+                           onclick="window.print()"/>
+                    <span style="float: right; font-size: small;">SRDD is {{ $_date }}</span>
+                </span>
+                <div id="calendar" class="text-slate-500 mx-4 mb-8 mt-2 p-4"></div>
+                @endif
+            </div>
 
-@section('content')
-<div class="container">
-    <x-srdd.nav-home />
-    {{-- check for any sessions for this user --}}
-    @if ($user->schedules->count() == 0) 
-    <x-srdd.notice :title="__('Nothing Scheduled')">
-        <span class="mb-8">
-            {{ __('You do not have any sessions currently scheduled') }}
-        </span>
-        <br/><br/>
-        <div lass="mt-6 mb-4 flex justify-end">
-            <a  class="px-4 py-2 
-                    bg-green-500 border border-green-300 rounded-md 
-                    font-semibold text-xs text-slate-200 uppercase tracking-widest 
-                    shadow-sm hover:bg-sky-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 
-                    disabled:opacity-25 transition ease-in-out duration-150"
-                href="{{ route('schedule.init') }}">Add Default Sessions</a>
-            <a  class="px-4 py-2 
-                    bg-sky-500 border border-sky-300 rounded-md 
-                    font-semibold text-xs text-slate-200 uppercase tracking-widest 
-                    shadow-sm hover:bg-sky-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 
-                    disabled:opacity-25 transition ease-in-out duration-150"
-                href="{{ route('calendar') }}">Goto Registration Page</a>
-        </div>
-        
-    </x-srdd.notice>
-    @else
-    <x-srdd.notice :title="__('Event Calendar')">
-        Here is a simplified version of your SRDD schedule that is suitable for printing or emailing.
-    </x-srdd.notice>
-    <div id="calendar" class="text-amber-900 mx-4 mb-8 mt-2 p-4 bg-amber-50 shadow-md"></div>
-    @endif
-</div>
-
-{{-- Add all the current sessions --}}
-<script> 
-    window.onload = function () {
-        var calendarEl = document.getElementById('calendar');
-        var calendar = new Calendar(calendarEl, {
-            customButtons: {
-                srddButton: {
-                    text: 'Return to SRDD',
-                    click: function() {
-                        calendar.gotoDate('{{ $srdd_date }}');
-                    }
+            {{-- Add all the current sessions --}}
+            <script> 
+                window.onload = function () {
+                    var calendarEl = document.getElementById('calendar');
+                    var calendar = new Calendar(calendarEl, {
+                        customButtons: {
+                            srddButton: {
+                                text: 'Return to SRDD',
+                                click: function() {
+                                    calendar.gotoDate('{{ $srdd_date }}');
+                                }
+                            }
+                        },
+                        plugins: [listPlugin],
+                        headerToolbar: {
+                            left: '',
+                            center: '',
+                            right:  '',
+                        },
+                        initialView: 'listDay',
+                        initialDate: '{{ $srdd_date }}',
+                        slotMinTime: '6:00:00',
+                        slotMaxTime: '21:00:00',
+                        {!! $events !!}
+                    });
+                    calendar.render();
                 }
-            },
-            plugins: [listPlugin],
-            headerToolbar: {
-                left: 'title',
-                center: '',
-                right:  '',
-            },
-            initialView: 'listDay',
-            initialDate: '{{ $srdd_date }}',
-            slotMinTime: '6:00:00',
-            slotMaxTime: '21:00:00',
-            {!! $events !!}
-        });
-        calendar.render();
-    }
-</script>
-@endsection
+            </script>
+        </main>
+</html>
