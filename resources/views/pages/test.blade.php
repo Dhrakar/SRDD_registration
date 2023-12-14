61b1d956c4a663f76434cb4e113a71dcbe9e8b62
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2023-12-13 19:44:32 -0900

    add tooltip test and bottom footer

--- resources/views/pages/test.blade.php
+++ resources/views/pages/test.blade.php
@@ -2,6 +2,12 @@
 @section('content')
 <div class="container h-view w-view">
     <x-srdd.nav-admin/>
+    <button class="bg-emerald-400 border border-emerald-950 rounded-md m-8 px-2 text-std" data-tippy-content="Tooltip">
+        Test of tooltip widget ...
+    </button>
+    <x-srdd.title-box :title="__('Title')">
+        <span class="text-std">Test of the Title Box widget ...</span>
+    </x-srdd.title-box>
     <x-srdd.callout>
         Test of the Callout widget ... 
     </x-srdd.callout>
@@ -22,11 +28,13 @@
     <x-danger-button
         x-data=""
         x-on:click.prevent="$dispatch('open-modal', 'confirm-deletion')"
-    >Do delete
+    >
+        Test Delete Button
     </x-danger-button>
     <x-modal name="confirm-deletion" focusable>
         <form method="get" action="/admin">
             <div class="mt-6 flex justify-end">
+                <span class="pr-4 text-std">Test of the confirmation dialog.  No actual deletes will happen</span>
                 <x-secondary-button x-on:click="$dispatch('close')">
                     {{ __('Cancel') }}
                 </x-secondary-button>
@@ -38,4 +46,5 @@
         </form>
     </x-modal>
 </div>
+<div class="mt-8 p-2">&nbsp;</div>
 @endsection
