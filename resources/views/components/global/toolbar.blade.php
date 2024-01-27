e8ad1097d7840dc3daab0709a6edd6bed717738b
Derek Bastille <63886752+Dhrakar@users.noreply.github.com>
2024-01-26 15:37:19 -0900

    add server tooltip to icon

--- resources/views/components/global/toolbar.blade.php
+++ resources/views/components/global/toolbar.blade.php
@@ -12,8 +12,9 @@
 <div class="bg-slate-700 text-sm font-bold py-1 w-screen" style="list-style-type: none;">
     <ui class="flex">
         <li class="mx-2">
-            <a class=""
+            <a  class=""
                 href="{{ $target }}"
+                title="{{ gethostname() }}"
             >
                 <i class="bi {{ $icon }} text-emerald-300"></i>
             </a>
