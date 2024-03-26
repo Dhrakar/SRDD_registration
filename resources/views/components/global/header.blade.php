432af3ec5e6750dae7048bd6245a445e08fcf546
dlbastille <dlbastille@alaska.edu>
2024-03-26 09:40:36 -0800

    update bootstrap icons version

--- resources/views/components/global/header.blade.php
+++ resources/views/components/global/header.blade.php
@@ -8,7 +8,7 @@
 
 <!-- fonts & icons --> 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;600;700&display=swap">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
 
 <!-- CSS for flatpickr -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
